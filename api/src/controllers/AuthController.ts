612a81dad1a27bef5555b1d08b9ce528e03feca6
ojay234 <davidace998@gmail.com>
2023-05-05 07:01:21 +0100

    updates

--- api/src/controllers/AuthController.ts
+++ api/src/controllers/AuthController.ts
@@ -2,12 +2,40 @@ import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User, { IUser } from "../models/User";
+import validator from "validator";
 
 export async function register(req: Request, res: Response): Promise<void> {
-  const { username, email, password } = req.body;
+  const { email, password } = req.body;
+
+  if (!email || !validator.isEmail(email)) {
+    res.status(400).json({ message: "Please enter a valid email address" });
+    return;
+  }
+
+  // Check if password is at least 8 characters long, contains at least one special character, and at least one uppercase character
+  if (
+    !password ||
+    password.length < 8 ||
+    !/[!@#$%^&*(),.?":{}|<>]/g.test(password) ||
+    !/[A-Z]/g.test(password)
+  ) {
+    let errorMessage =
+      "Password must be at least 8 characters long and include at least one uppercase letter and one special character";
+    if (!password) {
+      errorMessage = "Please enter a password";
+    } else if (password.length < 8) {
+      errorMessage = "Password must be at least 8 characters long";
+    } else if (!/[!@#$%^&*(),.?":{}|<>]/g.test(password)) {
+      errorMessage = "Password must include at least one special character";
+    } else if (!/[A-Z]/g.test(password)) {
+      errorMessage = "Password must include at least one uppercase letter";
+    }
+    res.status(400).json({ message: errorMessage });
+    return;
+  }
 
   try {
-    let user: IUser | null = await User.findOne({ email, username });
+    let user: IUser | null = await User.findOne({ email });
 
     if (user) {
       res.status(400).json({ message: "User already exists" });
@@ -15,13 +43,13 @@ export async function register(req: Request, res: Response): Promise<void> {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    user = await User.create({ username, email, password: hashedPassword });
+    user = await User.create({ email, password: hashedPassword });
 
     const token = jwt.sign(
       { userId: user._id },
       process.env.JWT_SECRET || "secret"
     );
-    res.status(200).json({ user: { username, email, token } });
+    res.status(200).json({ user: { email, token } });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
@@ -30,10 +58,20 @@ export async function register(req: Request, res: Response): Promise<void> {
 
 export async function login(req: Request, res: Response) {
   try {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
+
+    if (!email || !validator.isEmail(email)) {
+      res.status(400).json({ message: "Please enter a valid email address" });
+      return;
+    }
+
+    if (!password) {
+      res.status(400).json({ message: "Please enter password" });
+      return;
+    }
 
     // Check if the user exists
-    const user: IUser | null = await User.findOne({ username });
+    const user: IUser | null = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
@@ -47,7 +85,7 @@ export async function login(req: Request, res: Response) {
     // Generate a JWT token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!);
 
-    res.status(200).json({ user: { username, token } });
+    res.status(200).json({ user: { email, token } });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,17 +1,11 @@
 import { Schema, model, Document } from "mongoose";
 
 export interface IUser extends Document {
-  username: string;
   email: string;
   password: string;
 }
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
   email: {
     type: String,
     required: true,
